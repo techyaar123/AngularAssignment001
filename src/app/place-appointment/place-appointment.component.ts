082c772f9b0a862328bfa4adfcf044da2cfcba39
Abhinav Katiyar <abhinav.katiyar@happiestminds.com>
2022-11-20 16:51:36 +0530

    added amount and weeks logic

--- src/app/place-appointment/place-appointment.component.ts
+++ src/app/place-appointment/place-appointment.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,50 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './place-appointment.component.html',
   styleUrls: ['./place-appointment.component.css']
 })
-export class PlaceAppointmentComponent implements OnInit {
+export class PlaceAppointmentComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
+textBoxDisabled=false;
+amount:number=0;
+weeks:number=1;
+pack1:boolean=false;
+pack2:boolean=false;
+pack3:boolean=false;
+toggle(){
+  this.textBoxDisabled=false;
+  this.amount=400;
+  this.pack1=false;
+  this.pack3=false;
+  this.pack2=true;
+}
+toggle2(){
+  this.textBoxDisabled=true;
+  this.amount=500;
+  this.pack2=false;
+  this.pack3=false;
+  this.pack1=true;
+}
+toggle3(){
+  this.textBoxDisabled=false;
+  this.amount=300;
+  this.pack1=false;
+  this.pack2=false;
+  this.pack3=true;
+}
+onKey(event:any)
+{
+  if(event.target.value=="")
+  {
+    if(this.pack2==true)
+    {
+      this.amount=400;
+    }else if(this.pack3==true)
+    {
+      this.amount=300;
+    }
+    
+  }else{
+    this.amount=Number(event.target.value) * this.amount;
   }
-
+  
+}
 }
